a1de0071163cff140ee00f516f7fe07a67b7e976
Abel ko <abel.ko@rainist.com>
2020-08-24 02:06:36 +0900

    study switch

--- main.go
+++ main.go
@@ -40,6 +40,16 @@ func canIDrink(age int) bool {
 	return true
 }
 
+func canIDrinkWithSwitch(age int) bool {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
 func main() {
 	fmt.Println(multiply(123, 4234))
 
