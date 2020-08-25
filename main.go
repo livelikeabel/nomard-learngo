d55075719ec73a29306cc7779f567fe06b42a37f
Abel ko <abel.ko@rainist.com>
2020-08-25 16:55:35 +0900

    add delete method

--- main.go
+++ main.go
@@ -10,10 +10,8 @@ func main() {
 	dictionary := mydict.Dictionary{}
 	baseWord := "hello"
 	dictionary.Add(baseWord, "First")
-	err := dictionary.Update(baseWord, "Second")
-	if err != nil {
-		fmt.Println(err)
-	}
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
 	word, _ := dictionary.Search(baseWord)
 	fmt.Println(word)
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -43,3 +43,8 @@ func (d Dictionary) Update(word, def string) error {
 	}
 	return nil
 }
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
